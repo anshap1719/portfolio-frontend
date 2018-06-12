33a2cc1a7f39e48cd19f7f6b262f79f208835ba7
anshap1719 <anshap1719@gmail.com>
2018-06-12 22:00:03 +0530

    Fix Gramin Init To Home Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,19 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {NavigationEnd, Router} from '@angular/router';
 import {NgsRevealConfig} from 'ng-scrollreveal';
 import {StateService} from './services/state.service';
-
-declare var Granim: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent implements OnInit {
   isHome: boolean;
 
   @ViewChild('granim') granimElement: ElementRef;
 
-  constructor(private router: Router, config: NgsRevealConfig) {
+  constructor(private router: Router, config: NgsRevealConfig, private granim: StateService) {
     config.duration = 1200;
     config.easing = 'cubic-bezier(0.6, 0.2, 0.1, 1)';
     config.distance = '200px';
