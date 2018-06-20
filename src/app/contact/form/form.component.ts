cbbe4a2bb421446883825a878b3900ebb9370259
anshap1719 <anshap1719@gmail.com>
2018-06-20 20:48:28 +0530

    Fix Form Not Clearing After Message Being Sent

--- src/app/contact/form/form.component.ts
+++ src/app/contact/form/form.component.ts
@@ -33,6 +33,25 @@ export class FormComponent implements OnInit {
         setTimeout(() => {
           this.form.message = '';
         }, 4000);
+      }, error => {
+        if (error.status === 200) {
+          this.isFlying = false;
+          this.form.name = '';
+          this.form.email = '';
+          this.form.message = 'Thank You! Your message has been received. I usually respond within a few hours...';
+
+          console.log(this.form);
+
+          setTimeout(() => {
+            this.form.message = '';
+          }, 4000);
+        } else {
+          const temp = this.form.message;
+          this.form.message = 'Message not sent! Please try again...';
+          setTimeout(() => {
+            this.form.message = temp;
+          }, 4000);
+        }
       });
   }
 
