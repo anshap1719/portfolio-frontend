1493fee201e4beda13cd49275c96b0775d7bbd32
anshap1719 <anshap1719@gmail.com>
2018-06-20 20:18:34 +0530

    Add Contact Page, Add Form

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -8,8 +8,14 @@ export class ContactService {
   constructor(private http: HttpClient) { }
 
   submitForm(form: any) {
+    console.log(form);
+    const data = { 'form-name': 'contact', ...form };
+    let url = Object.keys(data).map(function(k) {
+      return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]);
+    }).join('&');
+    url = '/contact?' + url;
     return new Promise(resolve => {
-      this.http.post('/', encode({ 'form-name': 'contact', ...form }), {
+      this.http.post(url, null, {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
         }
