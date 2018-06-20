d88c34da8d7a2b3e54d9b2123022c5f81cf36a05
anshap1719 <anshap1719@gmail.com>
2018-06-20 20:24:41 +0530

    Add Contact Page, Add Form

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -8,14 +8,12 @@ export class ContactService {
   constructor(private http: HttpClient) { }
 
   submitForm(form: any) {
-    console.log(form);
     const data = { 'form-name': 'contact', ...form };
+
     let url = Object.keys(data).map(function(k) {
       return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]);
     }).join('&');
     url = '/contact?' + url;
-
-    console.log(url);
 
     return new Promise(resolve => {
       this.http.post(url, null, {
@@ -25,7 +23,6 @@ export class ContactService {
       })
         .subscribe(next => {
           resolve();
-          console.log(next);
         });
     });
   }
