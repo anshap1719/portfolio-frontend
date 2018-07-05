0910e95d04c2ae75f1a73ea211d5a0e55b7a55fc
Anshul Sanghi <anshap1719@gmail.com>
2018-07-05 21:48:03 +0530

    Add Form Validation To Contact Form

--- src/app/contact/form/form.component.ts
+++ src/app/contact/form/form.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ContactService} from '../../services/contact.service';
 import {DeviceService} from '../../services/device.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -14,7 +15,11 @@ export class FormComponent implements OnInit {
     email: '',
     message: '',
   };
+  invalidEmail;
   isMobile = false;
+  submitAttempt = false;
+
+  @ViewChild('formElem') formElem: NgForm;
 
   constructor(private contact: ContactService, device: DeviceService) {
     this.isMobile = device.isMobile();
@@ -23,36 +28,47 @@ export class FormComponent implements OnInit {
   ngOnInit() {
   }
 
-  sendForm() {
-    this.isFlying = true;
-    this.contact.submitForm(this.form)
-      .subscribe(next => {
-        this.isFlying = false;
-        this.form.name = '';
-        this.form.email = '';
-        this.form.message = 'Thank You! Your message has been received. I usually respond within a few hours...';
+  checkEmail() {
+    const regexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    this.invalidEmail = !regexp.test(this.form.email);
+  }
 
-        setTimeout(() => {
-          this.form.message = '';
-        }, 4000);
-      }, error => {
-        if (error.status === 200) {
+  sendForm() {
+    this.submitAttempt = true;
+    if (!this.formElem.invalid && (this.formElem.touched || this.formElem.dirty)) {
+      this.isFlying = true;
+      this.contact.submitForm(this.form)
+        .subscribe(next => {
           this.isFlying = false;
           this.form.name = '';
           this.form.email = '';
           this.form.message = 'Thank You! Your message has been received. I usually respond within a few hours...';
 
           setTimeout(() => {
+            this.submitAttempt = false;
             this.form.message = '';
           }, 4000);
-        } else {
-          const temp = this.form.message;
-          this.form.message = 'Message not sent! Please try again...';
-          setTimeout(() => {
-            this.form.message = temp;
-          }, 4000);
-        }
-      }).unsubscribe();
+        }, error => {
+          if (error.status === 200) {
+            this.isFlying = false;
+            this.form.name = '';
+            this.form.email = '';
+            this.form.message = 'Thank You! Your message has been received. I usually respond within a few hours...';
+
+            setTimeout(() => {
+              this.submitAttempt = false;
+              this.form.message = '';
+            }, 4000);
+          } else {
+            const temp = this.form.message;
+            this.form.message = 'Message not sent! Please try again... If you are unable to message using the form, please send an email instead.';
+            setTimeout(() => {
+              this.submitAttempt = false;
+              this.form.message = temp;
+            }, 4000);
+          }
+        }).unsubscribe();
+    }
   }
 
 }
